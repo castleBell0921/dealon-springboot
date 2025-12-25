cfe8e7d4aa63ee90ba8fafa31436af801d74aa6f
seonghyeon hwang <ghkd5370@naver.com>
2025-12-25 17:29:50 +0900

    25/12/25 슬픈 크리스마스.. 마이페이지에서도 리뷰작성 구현 완료

---
+++ src/main/resources/static/js/user/myBuyList.js
@@ -0,0 +1,31 @@
+document.addEventListener('click', async e => {
+  const reviewWriteBtn = e.target.closest('.review-write'); // 수정: closest로 정확히 찾기
+  const productLink = e.target.closest('.product-link');
+  const reviewNoForm = document.querySelector('#reviewNo');
+  if (reviewWriteBtn && productLink) {
+    e.stopPropagation();
+    e.preventDefault(); 
+
+    const reviewNo = productLink.dataset.reviewNo;
+	reviewNoForm.value = reviewNo;
+    console.log('리뷰 번호:', reviewNo);
+
+    if (!reviewNo) {
+      alert('리뷰 정보를 찾을 수 없습니다.');
+      return;
+    }
+
+    // targetItem 역할을 대신할 가짜 객체 생성
+    const fakeTargetItem = {
+      classList: {
+        contains: className => className === 'type-transaction-complete'
+      }
+    };
+
+    // header.js에 정의된 함수 호출 (리뷰 작성 모드로)
+    await fetchReviewDetails(reviewNo, fakeTargetItem);
+
+    // 모달 표시
+    reviewModal.style.display = 'flex';
+  }
+});

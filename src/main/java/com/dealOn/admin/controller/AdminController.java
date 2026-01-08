639ef1a9b47a0636ee3a6b7395bc797ca2ee35bd
seonghyeon hwang <ghkd5370@naver.com>
2026-01-08 17:12:20 +0900

    26/01/08 관리자페이지 유저 관리 페이지 뷰 구현

--- src/main/java/com/dealOn/admin/controller/AdminController.java
+++ src/main/java/com/dealOn/admin/controller/AdminController.java
@@ -1,9 +1,11 @@
 package com.dealOn.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -12,27 +14,32 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
 	
 	@GetMapping("main")
-	public String admAcc() {
+	public String admAcc(HttpServletRequest request, Model model) {
+		model.addAttribute("requestURI",request.getRequestURI());
 		return "admin/dashboard";
 	}
 	
 	@GetMapping("userMng")
-	public String joinUsrMng() {
+	public String joinUsrMng(HttpServletRequest request, Model model) {
+		model.addAttribute("requestURI",request.getRequestURI());
 		return "admin/userMng";
 	}
 	
 	@GetMapping("productMng")
-	public String joinProductMng() {
+	public String joinProductMng(HttpServletRequest request, Model model) {
+		model.addAttribute("requestURI",request.getRequestURI());
 		return "admin/productMng";
 	}
 
 	@GetMapping("decUser")
-	public String decUser() {
+	public String decUser(HttpServletRequest request, Model model) {
+		model.addAttribute("requestURI",request.getRequestURI());
 		return "admin/decUser";
 	}
 
 	@GetMapping("decProduct")
-	public String decProduct() {
+	public String decProduct(HttpServletRequest request, Model model) {
+		model.addAttribute("requestURI",request.getRequestURI());
 		return "admin/decProduct";
 	}
 }

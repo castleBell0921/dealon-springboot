3cbd68835fb52258ca33df620ce2346c7afcdddb
seonghyeon hwang <ghkd5370@naver.com>
2026-02-01 16:16:21 +0900

    26/02/01 웹소켓 오류 수정 및 버그 수정

--- src/main/resources/static/js/product/productDetail.js
+++ src/main/resources/static/js/product/productDetail.js
@@ -76,7 +76,7 @@ function toggleWishlist(productNo) {
         .then(result => {
             if (result === 'login_required') {
                 if(confirm("로그인이 필요한 서비스입니다.\n로그인 페이지로 이동하시겠습니까?")) {
-                    location.href = '/user/login';
+                    modal.style.display = 'block';
                 }
                 return;
             }
